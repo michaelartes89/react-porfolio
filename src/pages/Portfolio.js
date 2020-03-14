66b0c9de0f06c9832ac74cd4fc398df75be6ee52
MDBA <artes.mike@gmail.com>
2020-03-14 16:52:19 -0400

    adding hrefs for cards

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -18,7 +18,7 @@ function Portfolio() {
           <Col size="md-6">
             <Card
               image={"https://michaelartes89.github.io/Model-Portfolio/assets/images/project1pic.png"}
-              name={"Off-The-Menu (protype web app"}
+              name={"Off-The-Menu (JHU group project 1)"}
               github={<a href="https://github.com/JonoAugustine/off-the-menu" target="_blank"> View Here</a>}
               deployed={<a href="https://jonoaugustine.github.io/off-the-menu/" target="_blank"> View Here Site</a>}
             />
@@ -29,9 +29,9 @@ function Portfolio() {
 
          <Card
               image={"https://michaelartes89.github.io/Model-Portfolio/assets/images/stovetop.png"}
-              name={"Off-The-Menu (protype web app"}
-              github={<a href="https://github.com/JonoAugustine/off-the-menu" target="_blank"> View Here</a>}
-              deployed={<a href="https://jonoaugustine.github.io/off-the-menu/" target="_blank"> View Here Site</a>}
+              name={"StoveTop (JHU group project 2)"}
+              github={<a href="https://github.com/JEmnetu/Stovetop" target="_blank"> View Here</a>}
+              deployed={<a href="http://stovetop.herokuapp.com/" target="_blank"> View Here Site</a>}
             />
          </Col>
         </Row>
@@ -40,17 +40,17 @@ function Portfolio() {
           <Col size="md-6">
           <Card
               image={"https://michaelartes89.github.io/Model-Portfolio/assets/images/eatdaburgerpic.png"}
-              name={"Off-The-Menu (protype web app"}
-              github={<a href="https://github.com/JonoAugustine/off-the-menu" target="_blank"> View Here</a>}
-              deployed={<a href="https://jonoaugustine.github.io/off-the-menu/" target="_blank"> View Here Site</a>}
+              name={"MySql DB App (JHU class project)"}
+              github={<a href="https://github.com/michaelartes89/Eat-Da-Burger" target="_blank"> View Here</a>}
+              deployed={<a href="https://safe-waters-34488.herokuapp.com/" target="_blank"> View Here Site</a>}
             />
             </Col>
             <Col size="md-6">
               <Card
               image={"https://michaelartes89.github.io/Model-Portfolio/assets/images/dayplannerpic.png"}
-              name={"Off-The-Menu <br /> (protype web app"}
-              github={<a href="https://github.com/JonoAugustine/off-the-menu" target="_blank"> View Here</a>}
-              deployed={<a href="https://jonoaugustine.github.io/off-the-menu/" target="_blank"> View Here Site</a>}
+              name={"Day Planner App (JHU class project)"}
+              github={<a href="https://github.com/michaelartes89/Project5-DayPlanner" target="_blank"> View Here</a>}
+              deployed={<a href="https://michaelartes89.github.io/Project5-DayPlanner/" target="_blank"> View Here Site</a>}
             />
 
           </Col>
@@ -59,17 +59,17 @@ function Portfolio() {
           <Col size="md-6">
           <Card
               image={"https://michaelartes89.github.io/Model-Portfolio/assets/images/weatherdashpic.png"}
-              name={"Off-The-Menu (protype web app"}
-              github={<a href="https://github.com/JonoAugustine/off-the-menu" target="_blank"> View Here</a>}
-              deployed={<a href="https://jonoaugustine.github.io/off-the-menu/" target="_blank"> View Here Site</a>}
+              name={"Weather App (JHU class project)"}
+              github={<a href="https://github.com/michaelartes89/Project-6-WeatherDashboard-" target="_blank"> View Here</a>}
+              deployed={<a href="https://michaelartes89.github.io/Project-6-WeatherDashboard-/" target="_blank"> View Here Site</a>}
             />
             </Col>
             <Col size="md-6">
               <Card
               image={"https://michaelartes89.github.io/Model-Portfolio/assets/images/notetaker.png"}
-              name={"Off-The-Menu (protype web app"}
-              github={<a href="https://github.com/JonoAugustine/off-the-menu" target="_blank"> View Here</a>}
-              deployed={<a href="https://jonoaugustine.github.io/off-the-menu/" target="_blank"> View Here Site</a>}
+              name={"Note Taker App (JHU class project)"}
+              github={<a href="https://github.com/michaelartes89/Note-Taker-App" target="_blank"> View Here</a>}
+              deployed={<a href="https://peaceful-gorge-92743.herokuapp.com/" target="_blank"> View Here Site</a>}
             />
 
           </Col>
